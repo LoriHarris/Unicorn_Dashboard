830b2da500e220d7f154a319916b10814ead881a
User <lorieharris@gmail.com>
2019-03-13 13:07:44 -0400

    Gauge chart

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -14,12 +14,72 @@ function buildMetadata(sample) {
       cell.text(`${key}: ${value}`);
       
     console.log(`Key: ${key} | Value: ${value}`);
- 
-    });
-    
-  });
+    var WFREQ = (data.WFREQ*20)
 
-});
+      var traceA = [{ type: 'scatter',
+      x: [0], y:[0],
+       marker: {size: 28, color:'850000'},
+       showlegend: false,
+       name: 'speed',
+       text: data.WFREQ,
+       hoverinfo: 'text+name'},
+        {
+        
+        type: 'pie',
+        hole: .5,
+        rotation: 90,
+        values: [100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 /9, 100 / 9, 100 / 9, 100 / 9, 100 ],
+        text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+        direction: "clockwise",
+        textinfo: "text",
+        textposition: "inside",
+        marker: { colors: ["rgba(255, 0, 0, 0.6)", "rgba(255, 165, 0, 0.6)", "rgba(255, 255, 0, 0.6)", "rgba(144, 238, 144, 0.6)", "rgba(154, 205, 50, 0.6)", "rgba(255, 0, 0, 0.6)", "rgba(255, 165, 0, 0.6)", "rgba(255, 255, 0, 0.6)", "rgba(255, 165, 0, 0.6)", "white"]},
+          
+       
+        hoverinfo: "label"
+    
+      }];
+      
+      var degrees = 180- WFREQ;
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+// Path: may have to change to create a better triangle
+var mainPath = path1,
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    
+ 
+      var layout = {
+        shapes:[{
+            type: 'path',
+            path: path,
+            fillcolor: '850000',
+            line: {
+              color: '850000'
+            }
+          }],
+        title: 'Scrub Frequency',
+       
+        height: 500,
+        width: 500,
+        xaxis: {zeroline:false, showticklabels:false,
+                   showgrid: false, range: [-1, 1]},
+        yaxis: {zeroline:false, showticklabels:false,
+                   showgrid: false, range: [-1, 1]}
+      };
+      
+      Plotly.plot("gauge", traceA, layout, {staticPlot: true});
+        
+
+    });
+   }); 
+  });
 
 }
     // BONUS: Build the Gauge Chart
@@ -28,48 +88,46 @@ function buildMetadata(sample) {
 
 function buildCharts(sample) {
   d3.json(`/samples/${sample}`).then(function(data) {
-    var data = [data];
-    console.log(data);
-    var key1 = Object.values(data);
-    console.log(key1);
-  // Object.entries(data).forEach(([key,value]) => {
-  //   console.log(key, value);
-  key1.forEach((item) => {
-    var values1 = Object.values(key1);
-    console.log(values1)
-  var pie_data = [{
-    values: [values1[0], values1[1], values1[2]],
-    labels: ["ID", "Other", "Sample Type"],
+  var data = [{
+    values: data.sample_values.slice(0,10),
+    labels: data.otu_ids.slice(0,10),
+    hoverinfo: 'text+percent+name',
+    text: data.otu_labels.slice(0,10),
     type: 'pie'
   }];
-  var layout = {
-    height: 400,
-    width:500
-  };
-  Plotly.newPlot("pie", pie_data, layout);
-
-  });
-  });
-
-}
-//   var row = meta_chart.append("tbody");
-  //   Object.entries(data).forEach(([key, value]) => {
-  //     var cell = row.append("tr");
-      
-  //     cell.text(`${key}: ${value}`);
-      
-  //   console.log(`Key: ${key} | Value: ${value}`);
- 
-    // });
-    
   
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var layout = {
+    height: 500,
+    width: 500
+  };
+ 
+// Plot the chart to a div tag with id "plot"
+Plotly.newPlot("pie", data, layout);
 
-    // @TODO: Build a Bubble Chart using the sample data
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+
+
+d3.json(`/samples/${sample}`).then(function(data) {
+  console.log(data.sample_values)
+    var data1 = [{
+      x: data.otu_ids,
+      y: data.sample_values,
+      mode: 'markers',
+    }];
+
+    var layout = {
+      title: 'Marker Size',
+      showlegend: false,
+      height: 600,
+      width: 600
+    };
+    
+    Plotly.newPlot('bubble', data1, layout);
+  });  
+ 
+  });
+}
+
  
 
 function init() {
